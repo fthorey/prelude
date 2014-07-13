baff477a1ca877f5f6bf29aaffe1430da3561bc7
Florian Thorey <florian.thorey@gmail.com>
2014-07-13 17:30:43 +0200

    Add a personal config file

---
+++ personal/personal.el
@@ -0,0 +1,10 @@
+;;; personnal.el --- Emacs Personnal: Personnal configuration
+
+;; ---------------------------
+;; -- Custom functions --
+;; ---------------------------
+(defun reload ()
+  (interactive)
+  (load-file "~/.emacs.d/init.el"))
+
+(setq prelude-flyspell nil)
