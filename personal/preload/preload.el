d93cd03900879147f5a6857fc00960ea45d73208
Florian Thorey <florian.thorey@gmail.com>
2014-07-13 19:34:31 +0200

    Add a preload file to contain everything to load before prelude

---
+++ personal/preload/preload.el
@@ -0,0 +1,13 @@
+;;; preload.el --- Emacs Preload: Preload configuration
+
+;;; Commentary:
+
+;;; Code:
+
+;; Disable Flyspell (annoying)
+(setq prelude-flyspell nil)
+
+;; Disable guru-mode (annoying)
+(setq prelude-guru nil)
+
+;;; preload.el ends here
