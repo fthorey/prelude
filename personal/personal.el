a06c1ec90d1011c02de8b2a9a6e0558403adb155
Florian Thorey <florian.thorey@gmail.com>
2014-07-13 19:23:46 +0200

    Add Yasnippet support

--- personal/personal.el
+++ personal/personal.el
@@ -7,4 +7,15 @@
   (interactive)
   (load-file "~/.emacs.d/init.el"))
 
-(setq prelude-flyspell nil)
+;; Yasnippet
+(require 'yasnippet)
+(setq yas-snippet-dirs "~/.emacs.d/personal/snippets")
+(setq yas/wrap-around-region t)
+(setq yas/prompt-functions '(yas/x-prompt yas/ido-prompt))
+(yas/global-mode 1) ;;  make it global
+
+;; Cua mode
+(setq cua-enable-cua-keys nil) ;; only for rectangles
+(cua-mode t)
+
+;;; personal.el ends here
