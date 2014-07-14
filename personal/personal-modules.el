99eb7b08efa7352c57ed5ab133e646d40743e938
Florian Thorey <florian.thorey@gmail.com>
2014-07-14 11:35:47 +0200

    Add a personal-modules file to embed required packages

--- personal/personal-modules.el
+++ personal/personal-modules.el
@@ -1,2 +1,13 @@
+;;; personal-modules.el --- Emacs Personnal modules
+
+;;; Commentary:
+
+;;; Code:
+
 ;; Yasnippet
 (prelude-require-package 'yasnippet)
+
+;; Python environment, Jedi
+(prelude-require-packages '(python-environment jedi))
+
+;;; prelude-modules.el ends here
