aa0c7cd9693a2f4410bb0f0cc2542dda9eda8507
Florian Thorey <florian.thorey@gmail.com>
2014-07-14 11:34:19 +0200

    Rename preload to personal-preload

---
+++ personal/preload/personal-preload.el
@@ -0,0 +1,10 @@
+;;; personal-preload.el --- Emacs Personal Preload: Preload configuration
+
+;;; Commentary:
+
+;;; Code:
+
+;; Disable guru-mode (annoying)
+(setq prelude-guru nil)
+
+;;; personal-preload.el ends here
